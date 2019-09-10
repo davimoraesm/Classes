dabbd5f4c32876f92b094d7adf6df25349aaa3e0
davimoraesmm <davimoraesmiranda@gmail.com>
2019-09-09 23:55:39 -0300

    pouca coisa

--- Main.java
+++ Main.java
@@ -2,6 +2,17 @@ package com.classes;
 
 public class Main {
     public static void main(String[] args) {
+        //jeito 1 de declarar objeto
+        TextBox textBox1 = new TextBox();
 
+        //jeito 2 de declarar objeto
+        var textBox2 = new TextBox();
+
+        textBox2.setText("teste");
+        System.out.println(textBox2.text);
+        //textBox2.print();
+
+        var textBox3 = new TextBox();
+        textBox3.setText("Box 3");
     }
 }
--- TextBox.java
+++ TextBox.java
@@ -1,10 +1,14 @@
 package com.classes;
 
 public class TextBox {
-    public String text;
+    public String text = ""; // inicializar reference types pra nao ter risco de dar nullPointerException
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public void print(){
+        System.out.println(text);
     }
 
     public void clear() {
